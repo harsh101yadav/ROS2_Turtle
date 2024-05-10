06fc3aabd541ec22b3f3d9e187217626db09651f
harsh101yadav <harsh101yadav@gmail.com>
2024-05-11 01:56:05 +0530

    python node

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
 
@@ -6,6 +7,7 @@ def main(args = None):
     rclpy.init(args = args)
     node = Node("py_test")
     node.get_logger().info("Hello ROS2")
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == "__main__":
